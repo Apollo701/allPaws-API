8ab4530f9f4ae0aa964931e06bfea9cb8e694986
Jason Dorn <jdorn@mail.sfsu.edu>
2015-05-14 11:28:03 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Dog.Create([
+  {
+    name: "Walker",
+    breed: "Pit bull",
+    age: 2,
+    size: "medium",
+    trained: true,
+    animal_friendly: true,
+    child_friendly: true,
+    spayed_or_neutered: true
+    },
+  {
+    name: "Luke",
+    breed: "German Shepard",
+    age: 4,
+    size: "large",
+    trained: true,
+    animal_friendly: true,
+    child_friendly: true,
+    spayed_or_neutered: true
+    },
+  {
+    name: "Ember",
+    breed: "Austrailian Shepard",
+    age: 3,
+    size: "medium",
+    trained: true,
+    animal_friendly: true,
+    child_friendly: true,
+    spayed_or_neutered: true
+    },
+  {
+    name: "Vader",
+    breed: "Black Lab",
+    age: 1,
+    size: "medium",
+    trained: true,
+    animal_friendly: true,
+    child_friendly: true,
+    spayed_or_neutered: true
+    }
+    ])
