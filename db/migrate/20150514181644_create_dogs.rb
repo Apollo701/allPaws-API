35f6c99a72266c0e45378f01a99ee7c958dcada1
Jason Dorn <jdorn@mail.sfsu.edu>
2015-05-14 11:21:27 -0700

    created migration

--- db/migrate/20150514181644_create_dogs.rb
+++ db/migrate/20150514181644_create_dogs.rb
@@ -1,7 +1,16 @@
 class CreateDogs < ActiveRecord::Migration
   def change
     create_table :dogs do |t|
-
+      t.string  :name
+      t.string  :breed
+      t.integer :age
+      t.string  :size
+      t.string  :bio
+      t.string  :sex
+      t.boolean :trained
+      t.boolean :animal_friendly
+      t.boolean :child_friendly
+      t.boolean :spayed_or_neutered
       t.timestamps null: false
     end
   end
