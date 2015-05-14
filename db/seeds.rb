9736e671aed6ae6c27eb77e568203c112f2e2bef
Jason Dorn <jdorn@mail.sfsu.edu>
2015-05-14 11:29:49 -0700

    Seeded models and updated gem file with seralizer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Dog.Create([
+Dog.create([
   {
     name: "Walker",
     breed: "Pit bull",
