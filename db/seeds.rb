93f2aadd79b7c3a65c8e7d2c76c9f573731f81fd
Jason Dorn <jdorn@mail.sfsu.edu>
2015-05-14 18:19:03 -0700

    Added another seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,48 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Dog.create([
     {
-    name: "Daisy",
-    breed: "Pit bull",
+    name: "Dexter",
+    breed: "Corgi",
     age: 2,
     size: "medium",
-    bio: "Sweet as a rose",
-    sex: "female",
-    trained: true,
-    animal_friendly: true,
-    child_friendly: true,
-    spayed_or_neutered: true
-    },
-  {
-    name: "Luke",
-    breed: "German Shepard",
-    age: 4,
-    size: "large",
-    bio: "Courage in dog form",
-    sex: "male",
-    trained: true,
-    animal_friendly: true,
-    child_friendly: true,
-    spayed_or_neutered: true
-    },
-  {
-    name: "Ember",
-    breed: "Austrailian Shepard",
-    age: 3,
-    size: "medium",
-    bio: "She'll melt your heart",
-    sex: "female",
-    trained: true,
-    animal_friendly: true,
-    child_friendly: true,
-    spayed_or_neutered: true
-    },
-  {
-    name: "Vader",
-    breed: "Black Lab",
-    age: 1,
-    size: "medium",
-    bio: "Beware the dark side of the dog",
-    sex: "male",
+    bio: "Happy go lucky",
+    sex: "malex",
     trained: true,
     animal_friendly: true,
     child_friendly: true,
